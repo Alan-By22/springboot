4674b7754454ec5add322500e195995d827a2e8e
zby <zby@qq.com>
2022-12-08 15:56:40 +0800

    属性的映射

--- springboot-08-mybatisplus02/src/main/java/com/ithuipu/pojo/User.java
+++ springboot-08-mybatisplus02/src/main/java/com/ithuipu/pojo/User.java
@@ -1,5 +1,7 @@
 package com.ithuipu.pojo;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 /**
@@ -10,9 +12,23 @@ import lombok.Data;
  **/
 
 @Data
+//@TableName("t_user")
 public class User {
     private Long id;
     private String name;
+
+    /**
+     * 添加TableField,更改与数据库不匹配的字段
+     * 添加select ,不查询该字段
+     */
+    @TableField(value = "pwd",select = false)
     private String password;
+
     private Integer age;
+
+    /**
+     * 实体类中某些属性---不需要存储到数据库
+     */
+    @TableField(exist = false)
+    private Integer pic;
 }
